5f3075889834dd3b0e74c1bcfa46f19b6633167a
Wittawat Patcharinsak <kinkongjeng@gmail.com>
2018-04-14 15:04:56 +0700

    Transpile to old js syntax

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 function parseFields(fields) {
-    const res = {};
-    const keys = Object.keys(fields);
-    for (let i = 0, len = keys.length; i < len; i++) {
-        const key = keys[i];
-        res[key] = parseValue(fields[key]);
-    }
-    return res;
+  var res = {};
+  var keys = Object.keys(fields);
+  for (var i = 0, len = keys.length; i < len; i++) {
+    var key = keys[i];
+    res[key] = parseValue(fields[key]);
+  }
+  return res;
 }
 
 function parseValue(value) {
-    const type = Object.keys(value)[0];
+  var type = Object.keys(value)[0];
 
-    switch (type) {
-        case 'geoPointValue':
-            return [value.geoPointValue.latitude, value.geoPointValue.longitude];
-        case 'arrayValue':
-            return (value.arrayValue.values || []).map(parseValue);
-        case 'mapValue':
-            return parseFields(value.mapValue.fields);
-        case 'integerValue':
-        case 'doubleValue':
-            return Number(value[type]);
-        case 'timestampValue':
-            return new Date(value[type]);
-        default:
-            return value[type];
-    }
+  switch (type) {
+    case 'geoPointValue':
+      return [value.geoPointValue.latitude, value.geoPointValue.longitude];
+    case 'arrayValue':
+      return (value.arrayValue.values || []).map(parseValue);
+    case 'mapValue':
+      return parseFields(value.mapValue.fields);
+    case 'integerValue':
+    case 'doubleValue':
+      return Number(value[type]);
+    case 'timestampValue':
+      return new Date(value[type]);
+    default:
+      return value[type];
+  }
 }
 
 module.exports = parseFields;
